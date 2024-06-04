411d942dd8667422d9a1e753ddaabf41bd57a200
Dushusir <1414556676@qq.com>
2024-06-04 14:15:20 +0800

    Update math-kit.ts

--- math-kit.ts
+++ math-kit.ts
@@ -251,3 +251,38 @@ export function tolerateError(num: number, precision = 12) {
     const stripResult = strip(num, precision);
     return withinErrorMargin(num, stripResult, precision) ? stripResult : num;
 }
+
+
+/**
+ * Excel can display numbers with up to about 15 digits of precision. This includes the sum of the integer part and the decimal part
+ * @param input
+ * @returns
+ */
+export function truncateNumber(input: number | string): number {
+    const num = new Big(input);
+    const numStr = num.toFixed(); // Convert to fixed-point notation
+
+    const parts = numStr.split('.');
+    let integerPart = parts[0];
+    let decimalPart = parts.length > 1 ? parts[1] : '';
+
+    // Handle integer part greater than 15 digits
+    if (integerPart.length > 15) {
+        integerPart = integerPart.slice(0, 15) + '0'.repeat(integerPart.length - 15);
+    }
+
+    // Handle decimal part for numbers with an integer part of '0' and leading zeros
+    if (integerPart === '0') {
+        const nonZeroIndex = decimalPart.search(/[1-9]/); // Find the first non-zero digit
+        if (nonZeroIndex !== -1 && nonZeroIndex + 15 < decimalPart.length) {
+            decimalPart = decimalPart.slice(0, nonZeroIndex + 15);
+        }
+    } else if (integerPart.length + decimalPart.length > 15) {
+        // Adjust decimal part if total length exceeds 15
+        decimalPart = decimalPart.slice(0, 15 - integerPart.length);
+    }
+
+    // Convert back to number, may cause precision loss for very large or small numbers
+    return Number.parseFloat(integerPart + (decimalPart ? `.${decimalPart}` : ''));
+}
+
